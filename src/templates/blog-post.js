daec28b3711e4a92d1701c8e790eb381c6ace27f
mmuller88 <damadden88@googlemail.com>
2020-03-01 13:27:04 +0100

    commit function

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -45,6 +45,26 @@ function BlogPost(props) {
               <span>{date}</span>
               {image && <Img fluid={image.childImageSharp.fluid} />}
               <div dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }} />
+              <form name="contact" method="POST" data-netlify="true">
+              <p>
+                <label>Your Name: <input type="text" name="name" /></label>   
+              </p>
+              <p>
+                <label>Your Email: <input type="email" name="email" /></label>
+              </p>
+              <p>
+                <label>Your Role: <select name="role[]" multiple>
+                  <option value="leader">Leader</option>
+                  <option value="follower">Follower</option>
+                </select></label>
+              </p>
+              <p>
+                <label>Message: <textarea name="message"></textarea></label>
+              </p>
+              <p>
+                <button type="submit">Send</button>
+              </p>
+            </form>
               <div>
                   <span>Tagged in </span>
                   {tags.map((tag, i) => (
