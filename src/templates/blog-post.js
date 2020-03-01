be1262a527f2412a94aba9a0852aa098a3ecec4c
mmuller88 <damadden88@googlemail.com>
2020-03-01 14:12:11 +0100

    commit function

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -45,18 +45,12 @@ function BlogPost(props) {
               <span>{date}</span>
               {image && <Img fluid={image.childImageSharp.fluid} />}
               <div dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }} />
-              <form name="contact" method="POST" data-netlify="true">
+              <form name="contact" netlify netlify-honeypot="bot-field" hidden>
               <p>
                 <label>Your Name: <input type="text" name="name" /></label>   
               </p>
               <p>
                 <label>Your Email: <input type="email" name="email" /></label>
-              </p>
-              <p>
-                <label>Your Role: <select name="role[]" multiple>
-                  <option value="leader">Leader</option>
-                  <option value="follower">Follower</option>
-                </select></label>
               </p>
               <p>
                 <label>Message: <textarea name="message"></textarea></label>
