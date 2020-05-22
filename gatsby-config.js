e76fb37a32f1c1a3ae4547ab20ee49e829826a70
Martin Müller <damadden88@googlemail.com>
2020-05-22 13:59:57 +0200

    npm update + rss feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,95 @@ module.exports = {
     author: `@mmuller88`,
   },
   plugins: [
-    `gatsby-plugin-feed`,
+    {
+      resolve: 'gatsby-plugin-feed',
+        options: {
+          query: `
+            {
+              site {
+                siteMetadata {
+                  title
+                  description
+                  siteUrl
+                  site_url: siteUrl
+                }
+              }
+            }
+          `,
+          feeds: [
+            {
+              serialize: ({ query: { site, allMarkdownRemark } }) => {
+                return allMarkdownRemark.edges.filter(edge => edge.node.frontmatter.tags.includes("eng")).map(edge => {
+                  return Object.assign({}, edge.node.frontmatter, {
+                    description: edge.node.excerpt,
+                    date: edge.node.frontmatter.date,
+                    url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                    guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                    custom_elements: [{ "content:encoded": edge.node.html }],
+                  })
+                })
+              },
+              query: `
+                {
+                  allMarkdownRemark(
+                    sort: { order: DESC, fields: [frontmatter___date] },
+                  ) {
+                    edges {
+                      node {
+                        excerpt
+                        html
+                        fields { slug }
+                        frontmatter {
+                          title
+                          date
+                          tags
+                        }
+                      }
+                    }
+                  }
+                }
+              `,
+              output: "/rss.xml",
+              title: "Martin Mueller's Blog",
+            },
+            {
+              serialize: ({ query: { site, allMarkdownRemark } }) => {
+                return allMarkdownRemark.edges.map(edge => {
+                  return Object.assign({}, edge.node.frontmatter, {
+                    description: edge.node.excerpt,
+                    date: edge.node.frontmatter.date,
+                    url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                    guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                    custom_elements: [{ "content:encoded": edge.node.html }],
+                  })
+                })
+              },
+              query: `
+                {
+                  allMarkdownRemark(
+                    sort: { order: DESC, fields: [frontmatter___date] },
+                  ) {
+                    edges {
+                      node {
+                        excerpt
+                        html
+                        fields { slug }
+                        frontmatter {
+                          title
+                          date
+                          tags
+                        }
+                      }
+                    }
+                  }
+                }
+              `,
+              output: "/rss-ger.xml",
+              title: "Martin Mueller's Blog Ger",
+            },
+          ]
+        },
+    },
     'gatsby-plugin-react-helmet',
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
