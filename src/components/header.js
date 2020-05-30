b70c6c83e335c62c968c1bc84652ad5cb4744f65
Martin Müller <damadden88@googlemail.com>
2020-05-30 14:20:20 +0200

    Resume

--- src/components/header.js
+++ src/components/header.js
@@ -4,6 +4,7 @@ import React from "react"
 import twitterLogo from "../images/icons/twitter.svg"
 // import avatarIcon from "../images/avatarIcon.jpeg"
 import inLogo from "../images/icons/in.svg"
+import ghLogo from "../images/icons/gh.svg"
 
 const Header = ({ siteTitle }) => (
   <header
@@ -33,6 +34,7 @@ const Header = ({ siteTitle }) => (
       </h1>
       <div style={{color: "white", float:`right`, borderRadius: "25px"}}>
         <a href='https://martinmueller.dev/resume' target="_blank" rel='noopener'>Resume</a>
+        <a href='https://github.com/mmuller88' target="_blank" rel='noopener'><img  src={ghLogo} width='40' height='40' alt="twitter" style={{borderRadius: "25px"}}/> </a>
         <a href='https://www.twitter.com/MartinMueller_' target="_blank" rel='noopener'><img  src={twitterLogo} width='40' height='40' alt="twitter" style={{borderRadius: "25px"}}/> </a>
         <a href='https://www.linkedin.com/in/martinmueller88/' target="_blank" rel='noopener'><img  src={inLogo} width='40' height='40' alt="avatarIcon" style={{borderRadius: "25px"}}/> </a>
       </div>
