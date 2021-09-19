df48a8172ee667f62d883e383b7884b153be0ea1
Martin Mueller <damadden88@googlemail.com>
2021-09-19 07:38:16 +0200

    fix filter

--- src/templates/tag-template.js
+++ src/templates/tag-template.js
@@ -36,7 +36,7 @@ export const query = graphql`
 allMarkdownRemark(
       limit: 2000
       sort: { fields: [frontmatter___date], order: DESC }
-      filter: { frontmatter: { tags: { in: [$tag, "eng"] } } }
+      filter: { frontmatter: { tags: { eq: "eng" } } }
     ) {
       edges {
         node {
