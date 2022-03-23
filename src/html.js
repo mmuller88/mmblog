cefcba982d257f9e661410b0111077facfab7fe3
martinmueller.dev <damadden88@googlemail.com>
2022-03-23 19:15:24 +0100

    add ko-fi

--- src/html.js
+++ src/html.js
@@ -1,12 +1,11 @@
 import React from "react"
 import PropTypes from "prop-types"
-import KoFi from "./components/KoFi";
 
 export default function HTML(props) {
-  return (
-    <html {...props.htmlAttributes}>
-      <head>
-        {/* <script src='https://storage.ko-fi.com/cdn/scripts/overlay-widget.js'></script>
+ return (
+  <html {...props.htmlAttributes}>
+   <head>
+    {/* <script src='https://storage.ko-fi.com/cdn/scripts/overlay-widget.js'></script>
         <script>
           kofiWidgetOverlay.draw('martinmuellerdev', {
             'type': 'floating-chat',
@@ -15,33 +14,35 @@ export default function HTML(props) {
             'floating-chat.donateButton.text-color': '#fff'
           });
         </script> */}
-        
-        <meta charSet="utf-8" />
-        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no"
-        />
-        {props.headComponents}
-      </head>
-      <body {...props.bodyAttributes}>
-        {props.preBodyComponents}
-        <noscript key="noscript" id="gatsby-noscript">
-          This app works best with JavaScript enabled.
-        </noscript>
-        <div
-          key={`body`}
-          id="___gatsby"
-          dangerouslySetInnerHTML={{ __html: props.body }}
-        />
-        {props.postBodyComponents}
-      <footer>
-        <div id="amzn-assoc-ad-5831d393-b227-4ce9-b453-2f59df9559cc"></div><script async src="//z-na.amazon-adsystem.com/widgets/onejs?MarketPlace=US&adInstanceId=5831d393-b227-4ce9-b453-2f59df9559cc"></script>
-      </footer>
 
-      
+    <meta charSet="utf-8" />
+    <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+    <meta
+     name="viewport"
+     content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+    {props.headComponents}
+   </head>
+   <body {...props.bodyAttributes}>
+    {props.preBodyComponents}
+    <noscript key="noscript" id="gatsby-noscript">
+     This app works best with JavaScript enabled.
+    </noscript>
+    <div
+     key={`body`}
+     id="___gatsby"
+     dangerouslySetInnerHTML={{ __html: props.body }}
+    />
+    {props.postBodyComponents}
+    <footer>
+     <div id="amzn-assoc-ad-5831d393-b227-4ce9-b453-2f59df9559cc"></div>
+     <script
+      async
+      src="//z-na.amazon-adsystem.com/widgets/onejs?MarketPlace=US&adInstanceId=5831d393-b227-4ce9-b453-2f59df9559cc"
+     ></script>
+    </footer>
 
-      {/* <script src='https://storage.ko-fi.com/cdn/scripts/overlay-widget.js'></script>
+    {/* <script src='https://storage.ko-fi.com/cdn/scripts/overlay-widget.js'></script>
 <script>
   kofiWidgetOverlay.draw('martinmuellerdev', {
     'type': 'floating-chat',
@@ -50,16 +51,16 @@ export default function HTML(props) {
     'floating-chat.donateButton.text-color': '#fff'
   });
 </script> */}
-      </body>
-    </html>
-  )
+   </body>
+  </html>
+ )
 }
 
 HTML.propTypes = {
-  htmlAttributes: PropTypes.object,
-  headComponents: PropTypes.array,
-  bodyAttributes: PropTypes.object,
-  preBodyComponents: PropTypes.array,
-  body: PropTypes.string,
-  postBodyComponents: PropTypes.array,
+ htmlAttributes: PropTypes.object,
+ headComponents: PropTypes.array,
+ bodyAttributes: PropTypes.object,
+ preBodyComponents: PropTypes.array,
+ body: PropTypes.string,
+ postBodyComponents: PropTypes.array,
 }
