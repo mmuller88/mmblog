599dac1fd58d369f83dab6e98d9c212fd30701ed
martinmueller.dev <damadden88@googlemail.com>
2022-04-24 21:56:34 +0200

    remove prowler advertisement

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,18 @@
-import React from 'react'
-import { Link, graphql } from 'gatsby'
-import './post.css';
-import Layout from '../components/layout'
+import React from "react"
+import { Link, graphql } from "gatsby"
+import "./post.css"
+import Layout from "../components/layout"
 
 const IndexPage = (props) => {
-  const postList = props.data.posts;
-  //const data = props.data.tags.group;
-  return (
-    <Layout>
-      <div className="href">
-        <a href={`tags/de`}>de</a>, <a href={`tags/eng`}>eng</a>, <a href={`tags`}>tags</a>, <a href={`rss.xml`}>rss</a>, <a href={`rss-ger.xml`}>rss-ger</a>
-          {/* {
+ const postList = props.data.posts
+ //const data = props.data.tags.group;
+ return (
+  <Layout>
+   <div className="href">
+    <a href={`tags/de`}>de</a>, <a href={`tags/eng`}>eng</a>,{" "}
+    <a href={`tags`}>tags</a>, <a href={`rss.xml`}>rss</a>,{" "}
+    <a href={`rss-ger.xml`}>rss-ger</a>
+    {/* {
               data.filter((tag) => (tag.fieldValue === "eng" || tag.fieldValue === "de")).map(tag => (
                   
                   // <a href={`tags/${tag.fieldValue}`}>{tag.fieldValue} {`(${tag.totalCount})`}</a>
@@ -19,50 +21,53 @@ const IndexPage = (props) => {
                   // </Link>
               ))
           } */}
+   </div>
+   {/* <iframe width="560" height="315" src="https://www.youtube.com/embed/4JYaGylXEMc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */}
+   <div>
+    {postList.edges.map(({ node }, i) => (
+     <Link to={node.fields.slug} key={i} className="link">
+      <div className="post-list">
+       <h1>{node.frontmatter.title}</h1>
+       <span>{node.frontmatter.date}</span>
+       <p>{node.excerpt}</p>
       </div>
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/4JYaGylXEMc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-      <div>
-        {postList.edges.map(({ node }, i) => (
-          <Link to={node.fields.slug} key={i} className="link" >
-            <div className="post-list">
-              <h1>{node.frontmatter.title}</h1>
-              <span>{node.frontmatter.date}</span>
-              <p>{node.excerpt}</p>
-            </div>
-          </Link>
-        ))}
-      </div>
-    </Layout>
-  )
+     </Link>
+    ))}
+   </div>
+  </Layout>
+ )
 }
 
-export default IndexPage;
+export default IndexPage
 
 export const pageQuery = graphql`
-  query {
-    posts: allMarkdownRemark(
-      sort: { order: DESC, fields: [frontmatter___date] }
-      filter: { fileAbsolutePath: {regex : "\/content/"}, frontmatter: { show: { ne: "no" }} }
-      ) {
-      edges {
-        node {
-          fields{
-            slug
-          }
-          excerpt(pruneLength: 250)
-          frontmatter {
-            date(formatString: "Do MMMM YYYY")
-            title
-            show
-          }
-        }
-      }
+ query {
+  posts: allMarkdownRemark(
+   sort: { order: DESC, fields: [frontmatter___date] }
+   filter: {
+    fileAbsolutePath: { regex: "/content/" }
+    frontmatter: { show: { ne: "no" } }
+   }
+  ) {
+   edges {
+    node {
+     fields {
+      slug
+     }
+     excerpt(pruneLength: 250)
+     frontmatter {
+      date(formatString: "Do MMMM YYYY")
+      title
+      show
+     }
     }
-    tags: allMarkdownRemark(limit: 2000) {
-      group(field: frontmatter___tags) {
-        fieldValue
-        totalCount
-      }
-    }
+   }
   }
+  tags: allMarkdownRemark(limit: 2000) {
+   group(field: frontmatter___tags) {
+    fieldValue
+    totalCount
+   }
+  }
+ }
 `
