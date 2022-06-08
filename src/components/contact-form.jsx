ada24228516c1188a8a8364db7bb609d1e0f283e
Vsevolod <Vsevaseva@yandex.ru>
2022-06-08 15:59:49 +0300

    feat: setup form handling

--- src/components/contact-form.jsx
+++ src/components/contact-form.jsx
@@ -2,26 +2,25 @@ import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import tw, { styled } from 'twin.macro';
 import axios from 'axios';
-import { useStaticQuery, graphql } from 'gatsby';
 
 import { FullscreenBackground } from './background';
 
 export const CloseButton = styled.div`
-  ${tw`absolute z-20 top-12 right-6 w-8 h-8 opacity-50 
+ ${tw`absolute z-20 top-12 right-6 w-8 h-8 opacity-50 
   hover:opacity-100 hover:cursor-pointer before:bg-quaternary after:bg-quaternary`}
-  &:before, &:after {
-    position: absolute;
-    left: 15px;
-    content: ' ';
-    height: 33px;
-    width: 2px;
-  }
-  &:before {
-    transform: rotate(45deg);
-  }
-  &:after {
-    transform: rotate(-45deg);
-  }
+ &:before, &:after {
+  position: absolute;
+  left: 15px;
+  content: " ";
+  height: 33px;
+  width: 2px;
+ }
+ &:before {
+  transform: rotate(45deg);
+ }
+ &:after {
+  transform: rotate(-45deg);
+ }
 `;
 
 export const StyledForm = tw.form`flex flex-col w-5/6 md:w-1/3 mx-auto`;
@@ -47,27 +46,25 @@ export const ErrorMessage = tw.div`text-red-600 font-sans pt-4 text-center`;
 
 const ContactForm = ({ closeCallback }) => {
   const {
-    register, handleSubmit, formState: { errors },
+    register,
+    handleSubmit,
+    formState: { errors },
   } = useForm();
   const [showErrorMessage, toggleErrorMessage] = useState(false);
   const [showSuccessMessage, toggleSuccessMessage] = useState(false);
-  const data = useStaticQuery(graphql`
-    query contactFormQuery {
-      site {
-        siteMetadata {
-          formCallbackUrl
-        }
-      }
-    }
-  `);
-  const FormCallbackUrl = data.site.siteMetadata.formCallbackUrl || null;
+  const encode = (formData) => Object.keys(formData)
+    .map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(formData[key])}`)
+    .join('&');
   const onSubmit = (formData) => {
-    if (!FormCallbackUrl) {
-      console.log('please setup formCallBack url in your gatsby-config');
-      toggleSuccessMessage(true);
-      return;
-    }
-    axios.post(FormCallbackUrl, formData)
+    axios
+      .post('/', encode({
+        'form-name': 'contact',
+        ...formData,
+      }), {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      })
       .then((response) => {
         if (response.status === 200) {
           toggleErrorMessage(false);
@@ -86,18 +83,35 @@ const ContactForm = ({ closeCallback }) => {
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
     >
-      <StyledForm onSubmit={handleSubmit(onSubmit)}>
+      <StyledForm onSubmit={handleSubmit(onSubmit)} name="contact">
         <FormLabel htmlFor="name">Your name</FormLabel>
-        <FormInput name="name" placeholder="Name" {...register('name', { required: true })} />
+        <FormInput
+          name="name"
+          placeholder="Name"
+          {...register('name', { required: true })}
+        />
         {errors.name && <span tw="text-red-600">Name is required</span>}
         <FormLabel htmlFor="email">Your email</FormLabel>
-        <FormInput name="email" type="email" placeholder="Email" {...register('email', { required: true })} />
+        <FormInput
+          name="email"
+          type="email"
+          placeholder="Email"
+          {...register('email', { required: true })}
+        />
         {errors.email && <span tw="text-red-600">Email is required</span>}
         <FormLabel htmlFor="message">Your message</FormLabel>
         <FormText name="message" placeholder="Hi!" {...register('message')} />
-        {showSuccessMessage && (<SuccessMessage>Thanks for reaching out!</SuccessMessage>)}
-        {showSuccessMessage ? (<SubmitButton type="button" onClick={closeCallback} value="Close form" />) : (<SubmitButton type="submit" />)}
-        {showErrorMessage && (<ErrorMessage>Something went wrong. Try again</ErrorMessage>)}
+        {showSuccessMessage && (
+        <SuccessMessage>Thanks for reaching out!</SuccessMessage>
+        )}
+        {showSuccessMessage ? (
+          <SubmitButton type="button" onClick={closeCallback} value="Close form" />
+        ) : (
+          <SubmitButton type="submit" />
+        )}
+        {showErrorMessage && (
+        <ErrorMessage>Something went wrong. Try again</ErrorMessage>
+        )}
       </StyledForm>
       <CloseButton onClick={closeCallback} />
     </FullscreenBackground>
