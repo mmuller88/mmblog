1bb721c449e138e96985445709081ca09349a5b3
Martin Mueller <damadden88@googlemail.com>
2024-01-03 19:06:48 +0000

    try make picture responsive 6

--- gatsby-config.js
+++ gatsby-config.js
@@ -177,7 +177,23 @@ module.exports = {
     `gatsby-plugin-catch-links`,
     'gatsby-plugin-offline',
     {
-      resolve: "gatsby-transformer-remark",
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 590,
+            },
+          },
+        ],
+      },
+    },
+    // {
+    //   resolve: "gatsby-transformer-remark",
       // options: { // should be wrapped in options
       //   plugins: [
       //     {
@@ -192,7 +208,7 @@ module.exports = {
       //     "gatsby-remark-emoji",
       //   ],
       // }
-    },
+    // },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
